9746ad2c0465d82c302014175ff9b7e75b623a2b
5penquiry <enquiry@vpilot.org>
2026-08-17 01:48:54 +0530

    Refactor sessionStorage handling and render method

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
+
 import App from "./App.jsx";
 import "./index.css";
 
-const redirectPath = sessionStorage.getItem("prodsecops-redirect-path");
+const redirectPath = sessionStorage.getItem(
+  "prodsecops-redirect-path",
+);
+
 if (redirectPath) {
-  sessionStorage.removeItem("prodsecops-redirect-path");
-  window.history.replaceState(null, "", redirectPath);
+  sessionStorage.removeItem(
+    "prodsecops-redirect-path",
+  );
+
+  window.history.replaceState(
+    null,
+    "",
+    redirectPath,
+  );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>
+ReactDOM.createRoot(
+  document.getElementById("root"),
+).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
