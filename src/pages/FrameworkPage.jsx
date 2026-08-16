24949242be530f8f0e7686e91be17cacd1f4da7f
5penquiry <enquiry@vpilot.org>
2026-08-17 03:32:06 +0530

    Import EnterpriseFrameworkCommandCenter component

--- src/pages/FrameworkPage.jsx
+++ src/pages/FrameworkPage.jsx
@@ -5,6 +5,7 @@ import { domains } from "../data/domains";
 import ArchitectureOrb from "../components/framework/ArchitectureOrb";
 import ContextPanel from "../components/framework/ContextPanel";
 import FrameworkComponentVisuals from "../components/framework/FrameworkComponentVisuals";
+import EnterpriseFrameworkCommandCenter from "../components/framework/EnterpriseFrameworkCommandCenter";
 import FrameworkStructureDiagram from "../components/framework/FrameworkStructureDiagram";
 import StageRail from "../components/framework/StageRail";
 import GlowCard from "../components/common/GlowCard";
