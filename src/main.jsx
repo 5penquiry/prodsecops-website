d54a7f959543240d4764898350ea98bbf66d5cfa
5penquiry <enquiry@vpilot.org>
2026-08-17 13:00:48 +0530

    Add import for pso-v7-balloon.css

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import "./styles/enterprise-command-center.css";
 import "./styles/pso-precision-v4.css";
 import "./styles/pso-v5.css";
 import "./styles/pso-v6.css";
+import "./styles/pso-v7-balloon.css";
 
 const redirectPath = sessionStorage.getItem(
   "prodsecops-redirect-path",
