ce57084227f74dd435891112d5ea7fd2556b9f6b
5penquiry <enquiry@vpilot.org>
2026-08-18 20:14:29 +0530

    Add import for pso-v32-enterprise-framework.css

--- src/main.jsx
+++ src/main.jsx
@@ -34,6 +34,7 @@ import "./styles/pso-v28-footer-logo-contact-modal.css";
 import "./styles/pso-v29-why-prodsecops.css";
 import "./styles/pso-v30-why-prodsecops.css";
 import "./styles/pso-v31-why-prodsecops.css";
+import "./styles/pso-v32-enterprise-framework.css";
 
 
 const redirectPath = sessionStorage.getItem(
