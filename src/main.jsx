782ef74ddff6c8f45c4b5617baa290ff56f0886e
5penquiry <enquiry@vpilot.org>
2026-08-19 13:43:49 +0530

    Replace old enterprise framework styles with new ones

--- src/main.jsx
+++ src/main.jsx
@@ -34,8 +34,7 @@ import "./styles/pso-v28-footer-logo-contact-modal.css";
 import "./styles/pso-v29-why-prodsecops.css";
 import "./styles/pso-v30-why-prodsecops.css";
 import "./styles/pso-v31-why-prodsecops.css";
-import "./styles/pso-v32-enterprise-framework.css";
-import "./styles/pso-v35-enterprise-framework-complete.css";
+import "./styles/pso-v36-enterprise-framework.css";
 
 const redirectPath = sessionStorage.getItem(
   "prodsecops-redirect-path",
